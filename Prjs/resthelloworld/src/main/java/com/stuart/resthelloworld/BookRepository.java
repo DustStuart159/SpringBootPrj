00dcd26517e32f37310bc4e1b74a3f581c544258
DustStuart159 <ztrongnguyens96@gmail.com>
2020-11-17 17:29:02 +0700

    hoàn thành file REST Coding Overview
    mô tả các thành phần của REST Coding và các step xử lý

--- Prjs/resthelloworld/src/main/java/com/stuart/resthelloworld/BookRepository.java
+++ Prjs/resthelloworld/src/main/java/com/stuart/resthelloworld/BookRepository.java
@@ -2,5 +2,4 @@ package com.stuart.resthelloworld;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface BookRepository extends JpaRepository<Book, Long> {
-}
+public interface BookRepository extends JpaRepository<Book, Long> {}
