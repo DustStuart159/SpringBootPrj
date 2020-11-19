00b957f830c1f332badfcc99941bddb590f6b38c
DustStuart159 <ztrongnguyens96@gmail.com>
2020-11-20 00:53:08 +0700

    Change database location
    Change database location from memory to my sql

--- Prjs/resthelloworld/src/main/java/com/stuart/resthelloworld/Book.java
+++ Prjs/resthelloworld/src/main/java/com/stuart/resthelloworld/Book.java
@@ -3,58 +3,20 @@ package com.stuart.resthelloworld;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.math.BigDecimal;
+import lombok.Data;
 
 @Entity
+@Data
 public class Book {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String author;
     private BigDecimal price;
 
     // setters, getters, constructors, ...
-
-
-    public Book(String name, String author, BigDecimal price) {
-        this.name = name;
-        this.author = author;
-        this.price = price;
-    }
-
-    public Book() {    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
-    public BigDecimal getPrice() {
-        return price;
-    }
-
-    public void setPrice(BigDecimal price) {
-        this.price = price;
-    }
 }
--- Prjs/resthelloworld/src/main/java/com/stuart/resthelloworld/StartBookApplication.java
+++ Prjs/resthelloworld/src/main/java/com/stuart/resthelloworld/StartBookApplication.java
@@ -21,13 +21,4 @@ public class StartBookApplication extends SpringBootServletInitializer {
         SpringApplication.run(StartBookApplication.class, args);
     }
 
-    // init bean to insert 3 books into h2 database.
-    @Bean
-    CommandLineRunner initDatabase(BookRepository repository){
-        return args -> {
-            repository.save(new Book("A Guide to the Bodhisattva Way of Life", "Santideva", new BigDecimal("15.41")));
-            repository.save(new Book("The Life-Changing Magic of Tidying Up", "Marie Kondo", new BigDecimal("9.69")));
-            repository.save(new Book("Refactoring: Improving the Design of Existing Code", "Martin Fowler", new BigDecimal("47.99")));
-        };
-    }
 }
