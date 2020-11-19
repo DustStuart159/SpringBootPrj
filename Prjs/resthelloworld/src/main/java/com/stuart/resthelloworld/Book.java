4ff58aea4f52d33bb6ea98839183492c8771a39f
DustStuart159 <ztrongnguyens96@gmail.com>
2020-11-19 18:30:49 +0700

    Tạo mới prj [database]
    Tạo mới prj [database] trong nhánh BranchCompany

--- Prjs/resthelloworld/src/main/java/com/stuart/resthelloworld/Book.java
+++ Prjs/resthelloworld/src/main/java/com/stuart/resthelloworld/Book.java
@@ -1,7 +1,5 @@
 package com.stuart.resthelloworld;
 
-import jdk.nashorn.internal.objects.annotations.Getter;
-import jdk.nashorn.internal.objects.annotations.Setter;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
