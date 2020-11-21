eb17e5f4c17f85839649df6884cfb4a3cec86ef6
DustStuart159 <ztrongnguyens96@gmail.com>
2020-11-21 14:55:09 +0700

    thay đổi prj helloworld_service_db

--- Prjs/hello_rest_db/src/main/java/com/stuart/hello_rest_db/modul/ProductRepository.java
+++
@@ -1,6 +0,0 @@
-package com.stuart.hello_rest_db.modul;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product,Long> {
-}
