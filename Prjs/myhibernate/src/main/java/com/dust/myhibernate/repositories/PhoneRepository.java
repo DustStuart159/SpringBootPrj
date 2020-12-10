7b7288a0695c6ee7a11706a71c97bdc80f303720
DustStuart159 <ztrongnguyens96@gmail.com>
2020-12-10 23:28:09 +0700

    Revert "commit prj myhibernate"
    This reverts commit 25ec1d18a69830a444de45ab5b9f3563720bb1fa.

--- Prjs/myhibernate/src/main/java/com/dust/myhibernate/repositories/PhoneRepository.java
+++
@@ -1,9 +0,0 @@
-package com.dust.myhibernate.repositories;
-
-
-import com.dust.myhibernate.entities.Phone;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface PhoneRepository extends JpaRepository<Phone, Long> {
-}
